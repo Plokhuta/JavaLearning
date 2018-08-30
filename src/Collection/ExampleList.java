1726830d3af7feb668b9360bf33e475999b4c28d
Plokhuta <plokhuta.dima@gamil.com>
2018-08-30 17:48:49 +0300

    Collectoin List

---
+++ src/Collection/ExampleList.java
@@ -0,0 +1,29 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleList {
+    public static void main(String[] args) {
+        List<String> listStrings = new ArrayList<String>();
+
+        listStrings.add("one");
+        listStrings.add("two");
+
+        for(String item : listStrings){
+            System.out.println(item);
+        }
+
+        listStrings.remove(0);
+
+        try {
+            System.out.println(listStrings.get(1));
+        }catch (IndexOutOfBoundsException a){
+            System.err.println("Не існуючий елемент в списку!");
+        }
+
+        System.out.println(listStrings.get(0));
+
+    }
+
+}
